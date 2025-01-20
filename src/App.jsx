342fcd9826bffc530d1df582082eb3c3b25beeee
Misha999-max <63150319+Misha999-max@users.noreply.github.com>
2025-01-20 15:32:11 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,127 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/header/header'
-import Button from './components/button/Button'
-import Input from './components/input/Input'
-import ListItem from './components/listItem/ListItem'
+import { useState } from 'react';
+import './App.css';
+import Header from './components/header/header';
+import Button from './components/button/Button';
+import Input from './components/input/Input';
+import ListItem from './components/listItem/ListItem';
 
 function App() {
-	const [data, setDate] = useState([])
-	const [showDate, setShowDate] = useState([])
-	const [value, setValue] = useState('')
-	const [showAll, setShowAll] = useState(true)
+  const [data, setDate] = useState([]);
+  const [showDate, setShowDate] = useState([]);
+  const [value, setValue] = useState('');
+  const [valueSearch, setValueSearch] = useState('');
+  const [showAll, setShowAll] = useState(true);
 
-	function handleChange(value) {
-		setValue(value)
-	}
-	function handleClickAdd() {
-		if (value === '') return
-		const todos = {
-			text: value,
-			id: Date.now(),
-			isCompleted: false,
-			isActive: false,
-			timeAdded: new Date(),
-		}
-		setDate([...data, todos])
-		setValue('')
-	}
-	function handleShowDeletToDo() {
-		setShowAll(false)
-		setShowDate(data.filter(item => item.isCompleted === true))
-	}
-	function handleShowCompletedData() {
-		setShowAll(false)
-		setShowDate(data.filter(item => item.isActive === true))
-	}
-	function handleshowAllToDo() {
-		setShowAll(false)
-		setShowDate(data)
-	}
-	function handleDaleteClick(id) {
-		const showFilterDelete = data.filter(item => item.id !== id)
-		const isDeleteData = data.find(item => item.id === id)
-		if (isDeleteData) {
-			isDeleteData.isCompleted = !isDeleteData.isCompleted
-			if (isDeleteData.isActive) {
-				isDeleteData.isActive = !isDeleteData.isActive
-			}
-		}
-		setDate([...showFilterDelete, isDeleteData])
-	}
-	const handleChangeActive = id => {
-		const showFilterActive = data.filter(item => item.id !== id)
-		const isActiveData = data.find(item => item.id === id)
-		if (isActiveData) {
-			isActiveData.isActive = !isActiveData.isActive
-			if (isActiveData.isCompleted) {
-				isActiveData.isCompleted = !isActiveData.isCompleted
-			}
-		}
-		setDate([...showFilterActive, isActiveData])
-	}
-	return (
-		<div>
-			<Header />
-			<h1>Hello world!!!</h1>
-			<div className='main'>
-				<Input value={value} onChange={e => handleChange(e.target.value)} />
-				<div className='main-btn'>
-					<Button text='Добавить' onClick={handleClickAdd} />
-					<Button text='Активные' onClick={handleShowCompletedData} />
-					<Button text='Завершенные' onClick={handleShowDeletToDo} />
-					<Button text='All' onClick={handleshowAllToDo} />
-				</div>
-			</div>
-			<main>
-				<ul>
-					{showAll ? (
-						data.map(item => (
-							<ListItem
-								key={item.id}
-								item={item}
-								onDelete={handleDaleteClick}
-								onChangeActive={handleChangeActive}
-							/>
-						))
-					) : showDate.length > 0 ? (
-						showDate.map(item => (
-							<ListItem
-								key={item.id}
-								item={item}
-								onDelete={handleDaleteClick}
-								onChangeActive={handleChangeActive}
-							/>
-						))
-					) : (
-						<p className='ampty-string'>Здесь пока ни чего нЭЭЭЭЭЭту!!!</p>
-					)}
-				</ul>
-			</main>
-		</div>
-	)
+  function handleChange(value) {
+    setValue(value);
+  }
+
+  function handleSearchTodo(value) {
+    setValueSearch(value);
+    setShowDate(data.filter((item) => item.text.includes(value)));
+  }
+
+  function handleClickAdd() {
+    if (value === '') return;
+    const todos = {
+      text: value,
+      id: Date.now(),
+      isCompleted: false,
+      isActive: false,
+      timeAdded: new Date(),
+    };
+    setDate([...data, todos]);
+    setValue('');
+  }
+  function handleShowDeletToDo() {
+    setShowAll(false);
+    setShowDate(data.filter((item) => item.isCompleted === true));
+  }
+  function handleShowCompletedData() {
+    setShowAll(false);
+    setShowDate(data.filter((item) => item.isActive === true));
+  }
+  function handleshowAllToDo() {
+    setShowAll(false);
+    setShowDate(data);
+  }
+  function handleCompleteItem(id) {
+    const showFilterDelete = data.filter((item) => item.id !== id);
+    const isDeleteData = data.find((item) => item.id === id);
+    if (isDeleteData) {
+      isDeleteData.isCompleted = !isDeleteData.isCompleted;
+      if (isDeleteData.isActive) {
+        isDeleteData.isActive = !isDeleteData.isActive;
+      }
+    }
+    setDate([...showFilterDelete, isDeleteData]);
+  }
+
+  function hanldeDeleteItem(id) {
+    // console.log(data);
+    // console.log(id);
+    setDate(data.filter((item) => item.id !== id));
+    setShowAll(data);
+  }
+  const handleChangeActive = (id) => {
+    const showFilterActive = data.filter((item) => item.id !== id);
+    const isActiveData = data.find((item) => item.id === id);
+    if (isActiveData) {
+      isActiveData.isActive = !isActiveData.isActive;
+      if (isActiveData.isCompleted) {
+        isActiveData.isCompleted = !isActiveData.isCompleted;
+      }
+    }
+    setDate([...showFilterActive, isActiveData]);
+  };
+
+  return (
+    <div>
+      <Header />
+      <h1>Hello world!!!</h1>
+      <div className="main">
+        <Input value={value} onChange={(e) => handleChange(e.target.value)} />
+        <div className="main-btn">
+          <Button text="Добавить" onClick={handleClickAdd} />
+          <Button text="Активные" onClick={handleShowCompletedData} />
+          <Button text="Завершенные" onClick={handleShowDeletToDo} />
+          <Button text="All" onClick={handleshowAllToDo} />
+        </div>
+      </div>
+      <main>
+        <ul>
+          {showAll ? (
+            data.map((item) => (
+              <ListItem
+                key={item.id}
+                item={item}
+                onCompleted={handleCompleteItem}
+                onDelete={hanldeDeleteItem}
+                onChangeActive={handleChangeActive}
+              />
+            ))
+          ) : showDate.length > 0 ? (
+            showDate.map((item) => (
+              <ListItem
+                key={item.id}
+                item={item}
+                onCompleted={handleCompleteItem}
+                onDelete={hanldeDeleteItem}
+                onChangeActive={handleChangeActive}
+              />
+            ))
+          ) : (
+            <p className="ampty-string">Здесь пока ни чего нЭЭЭЭЭЭту!!!</p>
+          )}
+        </ul>
+
+        <div className="searcInput">
+          <h3>Поиск</h3>
+          <Input value={valueSearch} onChange={(e) => handleSearchTodo(e.target.value)} />
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,31 +1,18 @@
-import Button from '../button/Button'
-import './ListItem.css'
+import Button from '../button/Button';
+import './ListItem.css';
 
-export default function ListItem({ item, onDelete, onChangeActive }) {
-	return (
-		<li>
-			<div>
-				<p
-					className={
-						item.isCompleted
-							? 'delete-item'
-							: item.isActive
-							? 'active-item'
-							: ''
-					}
-				>
-					{item.text}
-				</p>
-				<span>Добавленно: {item.timeAdded.toLocaleTimeString()}</span>
-			</div>
-			<div>
-				<input
-					type='checkbox'
-					checked={item.isActive}
-					onChange={() => onChangeActive(item.id)}
-				/>
-				<Button text='Выполнено' onClick={() => onDelete(item.id)} />
-			</div>
-		</li>
-	)
+export default function ListItem({ item, onCompleted, onDelete, onChangeActive }) {
+  return (
+    <li>
+      <div>
+        <p className={item.isCompleted ? 'delete-item' : item.isActive ? 'active-item' : ''}>{item.text}</p>
+        <span>Добавленно: {item.timeAdded.toLocaleTimeString()}</span>
+      </div>
+      <div>
+        <input type="checkbox" checked={item.isActive} onChange={() => onChangeActive(item.id)} />
+        <Button text="Выполнено" onClick={() => onCompleted(item.id)} />
+        <Button className="deletItem" text="Delete" onClick={() => onDelete(item.id)} />
+      </div>
+    </li>
+  );
 }
